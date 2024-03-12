404257c79f8deac68b803ba68a93f285b26bfb04
hugogross <hugobombeirobbs@gmail.com>
2024-03-12 11:03:22 -0300

    asd

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,35 @@
       <router-view />
     </main>
     <footer>
+      <ul :class="menuAberto ? 'menu' : ''">
+        <li>Home</li>
+        <li>Eletrônicos</li>
+        <li>Jóias</li>
+        <li>Masculino</li>
+        <li>Feminino</li>
+      </ul>
       <p>Copyright &copy; 2024</p>
     </footer>
   </div>
 </template>
+
+<style scoped>
+footer {
+  display: flex;
+      flex-direction:column;
+      position: center;
+      background-color: rgba(0, 2, 121, 0.884);
+      box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+      border-radius: 10px;
+      right: 0;
+      text-align: right;
+      padding: 10px 16px;
+      align-items: center;
+      
+}
+footer p{
+  position: center;
+  display:flex;
+  color: rgb(255, 255, 255);
+}
+</style>
